9c2a8020110e3d1aeb7329cb7551c8d685f18dbd
Haqq Bassit <munagahmnh@gmail.com>
2023-08-08 17:46:12 +0000

    make correction for invalid input

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number:d} is", end=" ")
-number = str(number)
-if int(number) >= 0:
-    last_digit = int(number[-1])
+if number >= 0:
+    last_digit = number % 10
 else:
-    last_digit = int(number[-1])*-1
+    last_digit = int(str(number)[-1])
+print(f"Last digit of {number} is", end=" ")
 
 if last_digit > 5:
     print(f"{last_digit} and is greater than 5")
-elif int(number[-1]) == 0:
+elif last_digit == 0:
     print(f"{last_digit} and is 0")
 else:
     print(f"{last_digit} and is less than 6 and not 0")

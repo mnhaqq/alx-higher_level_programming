fb66879873233bef8811ce575ddb02534528a37c
Haqq Bassit <munagahmnh@gmail.com>
2023-08-13 19:49:45 +0000

    bug fix for empty strings

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     if not my_string:
-        return None
+        return ""
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             new_string += my_string[i]

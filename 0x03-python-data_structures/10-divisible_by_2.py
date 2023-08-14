e4da39011000fa7b1c80ea1619fb368c2da78231
Haqq Bassit <munagahmnh@gmail.com>
2023-08-14 18:08:36 +0000

    make change to access elements correctly

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             new.append(True)
         else:
             new.append(False)

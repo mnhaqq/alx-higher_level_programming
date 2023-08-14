405c4e33f9d1439d1db8767c3c9301cbf70a7223
Haqq Bassit <munagahmnh@gmail.com>
2023-08-14 17:36:00 +0000

    add solution for task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def divisible_by_2(my_list=[]):
+    new = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            new.append(True)
+        else:
+            new.append(False)
+    return new

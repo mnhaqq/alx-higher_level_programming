6d7e8c41d20a5928c48bdbe37463ab7c413776c3
Haqq Bassit <munagahmnh@gmail.com>
2023-08-14 17:59:50 +0000

    remove unnecessary condition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = []
-    if not my_list:
-        return new
     for i in range(len(my_list)):
         if i % 2 == 0:
             new.append(True)

121a79cfa0a867121db8b5e65a3af72840df509d
Haqq Bassit <munagahmnh@gmail.com>
2023-08-18 12:43:01 +0000

    fix bug for checking input type

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or roman_string is not str:
+    if not roman_string or type(roman_string) is not str:
         return 0
 
     dic = dict()

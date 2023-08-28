02789bd7952b8b1babb1e99c1470772b3b666f2a
Haqq Bassit <munagahmnh@gmail.com>
2023-08-28 16:43:07 +0000

    add solution to task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        ans = a/b
+    except ZeroDivisionError:
+        ans = None
+    finally:
+        print("Inside Result: {}".format(ans))

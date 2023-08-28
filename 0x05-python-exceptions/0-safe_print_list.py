5182df9b9bf8f2331ac3326ce623487f063e3943
Haqq Bassit <munagahmnh@gmail.com>
2023-08-28 16:09:02 +0000

    use while loop instead of for loop to keep better track of i

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(x):
+        while i < x:
             print(my_list[i], end="")
+            i += 1
     except IndexError:
-        i -= 1
-    finally:
-        print()
-        return i+1
+        pass
+    print()
+    return (i)

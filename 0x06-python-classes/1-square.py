9debd98ee099c8400e6a84848fd02bcab369baf7
Haqq Bassit <munagahmnh@gmail.com>
2023-08-29 23:05:06 +0000

    add solution to task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """A square
+
+    Defines a square with a size
+    """
+    def __init__(self, size):
+        self.__size = size
